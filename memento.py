19949047e9f4ef83d6df93807c573020c2e2156c
Edi Piqoni <piqoni@gmail.com>
2017-09-10 02:03:48 +0200

    first working version of memento

--- memento.py
+++ memento.py
@@ -1,27 +1,17 @@
-"""
-Inspired by http://code.activestate.com/recipes/578253-an-entry-with-autocompletion-for-the-tkinter-gui/
-Changes:
-    - Fixed AttributeError: 'AutocompleteEntry' object has no attribute 'listbox'
-    - Fixed scrolling listbox
-    - Case-insensitive search
-    - Added focus to entry field
-    - Custom listbox length, listbox width matches entry field width
-    - Custom matches function
-"""
-
-from tkinter import *
-import re
-
+#!/usr/bin/env python
+from Tkinter import Entry,Tk, GROOVE, StringVar, Listbox, END, ACTIVE, Label, LEFT, RIGHT
+import re, sys
+import csv
+import os
+import tkFont
+import clipboard
 
 class AutocompleteEntry(Entry):
     def __init__(self, autocompleteList, *args, **kwargs):
 
         # Listbox length
-        if 'listboxLength' in kwargs:
-            self.listboxLength = kwargs['listboxLength']
-            del kwargs['listboxLength']
-        else:
-            self.listboxLength = 8
+        self.listboxLength = kwargs.pop('listboxLength', 12)
+        self.listboxFontSize = tkFont.Font(size=18)
 
         # Custom matches function
         if 'matchesFunction' in kwargs:
@@ -34,6 +24,7 @@ class AutocompleteEntry(Entry):
 
             self.matchesFunction = matches
 
+
         Entry.__init__(self, *args, **kwargs)
         self.focus()
 
@@ -44,41 +35,64 @@ class AutocompleteEntry(Entry):
             self.var = self["textvariable"] = StringVar()
 
         self.var.trace('w', self.changed)
-        self.bind("<Right>", self.selection)
+        self.bind("<Return>", self.selection)
         self.bind("<Up>", self.moveUp)
         self.bind("<Down>", self.moveDown)
 
         self.listboxUp = False
 
+    def update_content_text(self, event):
+        w = event.widget
+        index = int(w.curselection()[0])
+        value = w.get(index)
+        clipboard_content = autocompleteList.get(value)
+        content['text'] = clipboard_content
+
     def changed(self, name, index, mode):
         if self.var.get() == '':
             if self.listboxUp:
+                content['text'] = ''
                 self.listbox.destroy()
                 self.listboxUp = False
         else:
             words = self.comparison()
             if words:
                 if not self.listboxUp:
-                    self.listbox = Listbox(width=self["width"], height=self.listboxLength)
-                    self.listbox.bind("<Button-1>", self.selection)
-                    self.listbox.bind("<Right>", self.selection)
+                    self.listbox = Listbox(width=self["width"], height=self.listboxLength, font= self.listboxFontSize)
+                    self.listbox.bind('<<ListboxSelect>>', self.update_content_text)
+                    self.listbox.bind("<Return>", self.selection)
                     self.listbox.place(x=self.winfo_x(), y=self.winfo_y() + self.winfo_height())
                     self.listboxUp = True
 
                 self.listbox.delete(0, END)
                 for w in words:
-                    self.listbox.insert(END, w)
+                    self.listbox.insert(END,w)
+                    self.listbox.see(0) # Scroll!
+                    self.listbox.selection_set(first=0)
+                    value = self.listbox.get(ACTIVE);
+
+                    clipboard_content = autocompleteList.get(value)
+                    content['text'] = clipboard_content
             else:
                 if self.listboxUp:
+                    content['text'] = ''
                     self.listbox.destroy()
                     self.listboxUp = False
 
     def selection(self, event):
         if self.listboxUp:
             self.var.set(self.listbox.get(ACTIVE))
+            value = self.listbox.get(ACTIVE);
+            clipboard_content = autocompleteList.get(value)
             self.listbox.destroy()
             self.listboxUp = False
             self.icursor(END)
+            self.copy_value(clipboard_content)
+            self.quit()
+
+    def copy_value(self, value):
+        clipboard.copy(value)
+
 
     def moveUp(self, event):
         if self.listboxUp:
@@ -91,9 +105,10 @@ class AutocompleteEntry(Entry):
                 self.listbox.selection_clear(first=index)
                 index = str(int(index) - 1)
 
-                self.listbox.see(index)  # Scroll!
+                self.listbox.see(index) # Scroll!
                 self.listbox.selection_set(first=index)
                 self.listbox.activate(index)
+            self.listbox.event_generate("<<ListboxSelect>>", when="tail")
 
     def moveDown(self, event):
         if self.listboxUp:
@@ -106,58 +121,74 @@ class AutocompleteEntry(Entry):
                 self.listbox.selection_clear(first=index)
                 index = str(int(index) + 1)
 
-                self.listbox.see(index)  # Scroll!
+                self.listbox.see(index)
                 self.listbox.selection_set(first=index)
                 self.listbox.activate(index)
+            self.listbox.event_generate("<<ListboxSelect>>", when="tail")
+
+
+    def quit(self):
+        root.quit()
 
     def comparison(self):
-        return [w for w in self.autocompleteList if self.matchesFunction(self.var.get(), w)]
+        return [ w for w in self.autocompleteList if self.matchesFunction(self.var.get(), w) ]
 
+def center(toplevel):
+    toplevel.update_idletasks()
+    w = toplevel.winfo_screenwidth()
+    h = toplevel.winfo_screenheight()
+    size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
+    x = w/2 - size[0]/2
+    y = h/2 - size[1]/2
+    toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
+
+def matches(fieldValue, acListEntry):
+    pattern = re.compile('.*' + re.escape(fieldValue) + '.*', re.IGNORECASE)
+    return re.match(pattern, acListEntry)
+
+def close(event):
+    root.destroy()
+
+def center_splash_screen(root, width, height):
+    w = root.winfo_screenwidth()
+    h = root.winfo_screenheight()
+    x = w / 2 - width / 2
+    y = h / 2 - height / 2
+    root.geometry("%dx%d+%d+%d" % ((width, height) + (x, y)))
 
 if __name__ == '__main__':
-    autocompleteList = ['Dora Lyons (7714)', 'Hannah Golden (6010)', 'Walker Burns (9390)', 'Dieter Pearson (6347)',
-                        'Allen Sullivan (9781)', 'Warren Sullivan (3094)', 'Genevieve Mayo (8427)',
-                        'Igor Conner (4740)', 'Ulysses Shepherd (8116)', 'Imogene Bullock (6736)',
-                        'Dominique Sanchez (949)', 'Sean Robinson (3784)', 'Diana Greer (2385)',
-                        'Arsenio Conrad (2891)', 'Sophia Rowland (5713)', 'Garrett Lindsay (5760)', 'Lacy Henry (4350)',
-                        'Tanek Conley (9054)', 'Octavia Michael (5040)', 'Kimberly Chan (1989)',
-                        'Melodie Wooten (7753)', 'Winter Beard (3896)', 'Callum Schultz (7762)',
-                        'Prescott Silva (3736)', 'Adena Crane (6684)', 'Ocean Schroeder (2354)', 'Aspen Blevins (8588)',
-                        'Allegra Gould (7323)', 'Penelope Aguirre (7639)', 'Deanna Norman (1963)',
-                        'Herman Mcintosh (1776)', 'August Hansen (547)', 'Oscar Sanford (2333)', 'Guy Vincent (1656)',
-                        'Indigo Frye (3236)', 'Angelica Vargas (1697)', 'Bevis Blair (4354)', 'Trevor Wilkinson (7067)',
-                        'Kameko Lloyd (2660)', 'Giselle Gaines (9103)', 'Phyllis Bowers (6661)', 'Patrick Rowe (2615)',
-                        'Cheyenne Manning (1743)', 'Jolie Carney (6741)', 'Joel Faulkner (6224)',
-                        'Anika Bennett (9298)', 'Clayton Cherry (3687)', 'Shellie Stevenson (6100)',
-                        'Marah Odonnell (3115)', 'Quintessa Wallace (5241)', 'Jayme Ramsey (8337)',
-                        'Kyle Collier (8284)', 'Jameson Doyle (9258)', 'Rigel Blake (2124)', 'Joan Smith (3633)',
-                        'Autumn Osborne (5180)', 'Renee Randolph (3100)', 'Fallon England (6976)',
-                        'Fallon Jefferson (6807)', 'Kevyn Koch (9429)', 'Paki Mckay (504)', 'Connor Pitts (1966)',
-                        'Rebecca Coffey (4975)', 'Jordan Morrow (1772)', 'Teegan Snider (5808)',
-                        'Tatyana Cunningham (7691)', 'Owen Holloway (6814)', 'Desiree Delaney (272)',
-                        'Armand Snider (8511)', 'Wallace Molina (4302)', 'Amela Walker (1637)', 'Denton Tillman (201)',
-                        'Bruno Acevedo (7684)', 'Slade Hebert (5945)', 'Elmo Watkins (9282)', 'Oleg Copeland (8013)',
-                        'Vladimir Taylor (3846)', 'Sierra Coffey (7052)', 'Holmes Scott (8907)',
-                        'Evelyn Charles (8528)', 'Steel Cooke (5173)', 'Roth Barrett (7977)', 'Justina Slater (3865)',
-                        'Mara Andrews (3113)', 'Ulla Skinner (9342)', 'Reece Lawrence (6074)', 'Violet Clay (6516)',
-                        'Ainsley Mcintyre (6610)', 'Chanda Pugh (9853)', 'Brody Rosales (2662)', 'Serena Rivas (7156)',
-                        'Henry Lang (4439)', 'Clark Olson (636)', 'Tashya Cotton (5795)', 'Kim Matthews (2774)',
-                        'Leilani Good (5360)', 'Deirdre Lindsey (5829)', 'Macy Fields (268)', 'Daniel Parrish (1166)',
-                        'Talon Winters (8469)']
-
-
-    def matches(fieldValue, acListEntry):
-        pattern = re.compile(re.escape(fieldValue) + '.*', re.IGNORECASE)
-        return re.match(pattern, acListEntry)
-
 
     root = Tk()
-    entry = AutocompleteEntry(autocompleteList, root, listboxLength=6, width=32, matchesFunction=matches)
+    root.minsize(width=200, height=390)
+    root.wait_visibility(root)
+    root.wm_attributes('-alpha',0.8)
+    root.wm_attributes('-type', 'combo')
+    center_splash_screen(root, 1000, 300)
+
+    root.bind('<Escape>', close)
+    root.bind('<Control-w>', close)
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    # JSON
+    # with open(dir_path + '/taskdata.json') as data_file:
+    #     autocompleteList = { d['id'] : d['value'] for d in loads(data_file.read())}
+
+    with open(dir_path + '/memento.csv') as data_file:
+        autocompleteList = {rows[0]:rows[1] for rows in csv.reader(data_file)}
+
+    search_list = list(autocompleteList.keys())
+
+    content = Label(root,
+           justify=LEFT,
+           padx = 10,
+           pady = 10,
+           width = 50,
+           wraplength = 250,
+           font=("Helvetica", 13),
+           text='')
+
+    entry = AutocompleteEntry(search_list, root, listboxLength=12, width=35, matchesFunction=matches, relief=GROOVE, font = "Helvetica 20", justify="center",bg="white",fg="black",disabledbackground="#1E6FBA",disabledforeground="white",highlightbackground="black",highlightcolor="white",highlightthickness=1,bd=0)
     entry.grid(row=0, column=0)
-    Button(text='Python').grid(column=0)
-    Button(text='Tkinter').grid(column=0)
-    Button(text='Regular Expressions').grid(column=0)
-    Button(text='Fixed bugs').grid(column=0)
-    Button(text='New features').grid(column=0)
-    Button(text='Check code comments').grid(column=0)
+    content.grid(rowspan=10, column=1)
     root.mainloop()
